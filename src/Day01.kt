e9c621530c43e695e3211a218bb8df963e1bcc9c
MatthewS8 <matthew-stefanelli@outlook.it>
2023-12-01 16:25:15 +0100

    Solution Day 1 So far...

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,116 @@
+import java.lang.Integer.max
+import kotlin.math.min
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        input.forEach {
+            sum += getNumberFromString(it)
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        input.forEach {
+            sum += getNumberDigitsFromString(it)
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
+
+fun getNumberFromString(string: String): Int {
+    var i = 0
+    var j = string.length - 1
+    var res = 0
+    var foundI = false
+    var foundJ = false
+    while (!foundI || !foundJ && i <= j) {
+        var tmp: Int?
+        if (!foundI) {
+            tmp = string[i].digitToIntOrNull()
+            if (tmp !== null) {
+                res += tmp * 10
+                foundI = true
+            } else {
+                i++
+            }
+        }
+        if (!foundJ) {
+            tmp = string[j].digitToIntOrNull()
+            if (tmp !== null) {
+                res += tmp
+                foundJ = true
+            } else {
+                j--
+            }
+        }
+    }
+    return res
+}
+fun getNumberDigitsFromString(string: String): Int {
+    val digits = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9,
+    )
+    var i = 0
+    var j = string.length - 1
+    var res = 0
+    var foundI = false
+    var foundJ = false
+    while (!foundI || !foundJ && i <= j) {
+        var tmp: Int?
+        if (!foundI) {
+            tmp = string[i].digitToIntOrNull()
+            if (tmp !== null) {
+                res += tmp * 10
+                foundI = true
+            } else {
+                val subs = string.substring(i, min( i+5, string.length))
+                for( (d, k) in digits) {
+                    if (subs.contains(d)) {
+                        res += k * 10
+                        foundI = true
+                        break
+                    }
+                }
+                i++
+            }
+        }
+        if (!foundJ) {
+            tmp = string[j].digitToIntOrNull()
+            if (tmp !== null) {
+                res += tmp
+                foundJ = true
+            } else {
+                val subs = string.substring(max(0, j-4), j + 1)
+                for( (d, k) in digits) {
+                    if (subs.contains(d)) {
+                        res += k
+                        foundJ = true
+                        break
+                    }
+                }
+                j--
+            }
+        }
+    }
+    println("for ${string} res is ${res}")
+    return res
+}
+
