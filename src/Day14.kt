245f5f7ead56ba58552be149a7af15b8a4646679
MatthewS8 <matthew-stefanelli@outlook.it>
2023-12-14 23:40:19 +0100

    Day 14 solution so far.

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,26 +1,76 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        val map: MutableMap<Int, Pair<Int, Char>> = mutableMapOf()
-        input.mapIndexed { rowIndex, row ->
+        val map: MutableMap<Int, MutableList<Pair<Int, Char>>> = mutableMapOf()
+         input.forEachIndexed { rowIndex, row ->
             val elements = row.mapIndexed { colIndex, char ->
                 char to colIndex
             }
-            elements.forEach {
-                map[it.second] = rowIndex to it.first
+            elements.forEach {(char, colIndex) ->
+                val list = map[colIndex]
+                if (list != null) {
+                    list.add(rowIndex to char)
+                } else {
+                    map[colIndex] = mutableListOf(rowIndex to char)
+                }
             }
+        }
 
+        val colsStrings: MutableList<String> = mutableListOf()
+        for (j in input[0].indices){
+            var col = ""
+            for(i in input.indices) {
+                col += input[i][j]
+            }
+            colsStrings.add(j, col)
         }
-        map.forEach { colIndex, u ->
-            // TODO: continue from here 
+
+        var sum = 0
+        colsStrings.forEach {col ->
+            val matchResult = col.split( '#')
+            var countedSoFar = col.indexOfFirst { it != '#' }
+            matchResult.forEachIndexed { i, str ->
+                if (str.isNotEmpty()) {
+                    val nOfOs = str.count { it == 'O' }
+                    for (ind in 0..<nOfOs)
+                        sum += col.length - ind + countedSoFar
+
+                    countedSoFar += str.length
+                } else {
+                    countedSoFar ++
+                }
+            }
         }
-        return 0
+
+        /*map.forEach {
+            val tmp = it.value.groupBy { it.second }
+            val rr = tmp['O']
+            val csr = tmp['#']
+            val sand = tmp['.']?.toMutableList()
+            var sum = 0
+            sum = rr?.sumOf { (colIndex, _) ->
+                var firstAvailableSpot = sand?.firstOrNull { (s, _) -> s < colIndex }
+                if (firstAvailableSpot != null) {
+                    val barrier = csr?.firstOrNull {(cubeRocksIndex, _) ->
+                        cubeRocksIndex < colIndex && cubeRocksIndex > firstAvailableSpot.first
+                    }
+                    if (barrier != null) {
+
+                    }
+                }
+            }
+        }*/
+
+        return sum.also(::println)
     }
 
     fun part2(input: List<String>): Int {
         return 0
     }
 
-    val input = readInput("DayXX")
+    val testInput = readInput("Day14_test")
+    check(part1(testInput) == 136)
+
+    val input = readInput("Day14")
     println(part1(input))
     println(part2(input))
 }
