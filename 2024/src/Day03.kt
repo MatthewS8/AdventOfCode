5bfa3f2f41b143a0b6967c8f33dbd2fe1bd706c0
MatthewS8 <matthew-stefanelli@outlook.it>
2024-12-04 05:52:04 +0100

    fix(Day3) fixed multiline issue

--- 2024/src/Day03.kt
+++ 2024/src/Day03.kt
@@ -1,24 +1,23 @@
 fun main() {
     fun part1(input: List<String>): Int {
         val mulRegex = Regex("""mul\(\d{1,3},\d{1,3}\)""")
-        return input.map { row ->
+        return input.flatMap { row ->
             mulRegex.findAll(row).map { it.value }.toList()
-        }.flatten().map {
+        }.map {
             val (a, b) = it.drop(4).dropLast(1).split(",").map { it.toInt() }
             a * b
         }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        // FIX: This won't work for multi-line input. Keep track of the last command of a row.
         val mulRegex = Regex("""mul\(\d{1,3},\d{1,3}\)""")
         val cmdRegex = Regex("""do(?:n't)?""")
-        val mulList =  input.map { row ->
+        val mulList =  input.flatMap { row ->
             mulRegex.findAll(row).map { it.value to it.range }.toList()
-        }.flatten()
-        val cmdList = input.map { row ->
+        }
+        val cmdList = input.flatMap { row ->
             cmdRegex.findAll(row).map { it.value to it.range }.toList()
-        }.flatten()
+        }
         return mulList.map { (mul, mulRange) ->
             val appliedCommand = cmdList.findLast { (_, cmdRange) -> cmdRange.first < mulRange.first }
             if (appliedCommand != null) {
