0ef5a400eaa61d042e9b7c55256f11394541d3c7
MatthewS8 <matthew-stefanelli@outlook.it>
2025-12-02 20:29:09 +0100

    Day 2 part 2

--- 2025/src/Day02.kt
+++ 2025/src/Day02.kt
@@ -22,16 +22,33 @@ fun main() {
         }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val ranges = prepareInput(input)
+        return ranges.sumOf { range ->
+            range.sumOf { id ->
+                val current = id.toString()
+                var chunkSize = 1
+                var isInvalid = false
+                while (chunkSize <= current.length / 2 && !isInvalid) {
+                    isInvalid = current.chunked(chunkSize).distinct().size == 1
+                    chunkSize++
+                }
+                if (isInvalid) {
+                    id
+                }
+                else 0
+            }
+        }
     }
 
-    // Or read a large test input from the `src/Day02_test.txt` file:
+    // Read a large test input from the `src/Day02_test.txt` file:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 1227775554L)
+    check(part2(testInput) == 4174379265L)
 
+    part2(testInput).println()
     // Read the input from the `src/Day02.txt` file.
     val input = readInput("Day02")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
